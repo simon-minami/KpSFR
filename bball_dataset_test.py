69737df3b2f805cfa869c321d6ea427ca17afa86
simon minami <simon.sho.minami@gmail.com>
2024-04-10 13:55:46 -0400

    stuff

--- bball_dataset_test.py
+++ bball_dataset_test.py
@@ -30,23 +30,58 @@ for frame, homo in zip(frames, homographies):
     court_corners = np.array([
         [0,0], [94, 0], [94, 50], [0, 50]
     ], dtype=float)
+    right_key = np.array([
+        (75, 19), (94, 19), (94, 31), (75, 31)
+    ], dtype=float)
+    half_court = np.array([
+        (47, 0), (47, 50)
+    ], dtype=float)
+
     court_corners = court_corners.reshape(-1, 1, 2)  # need to reshape for transformation
+    right_key = right_key.reshape(-1, 1, 2)
+    half_court = half_court.reshape(-1, 1, 2)
+
     court_corners_video = cv2.perspectiveTransform(court_corners, court_to_video)
+    right_key_video = cv2.perspectiveTransform(right_key, court_to_video)
+    half_court_video = cv2.perspectiveTransform(half_court, court_to_video)
+    # print(court_corners_video, court_corners_video.shape)
+
     court_corners_video = court_corners_video.astype(int).reshape(-1, 2)
-    print(court_corners_video, court_corners_video.shape)
+    right_key_video = right_key_video.astype(int).reshape(-1, 2)
+    half_court_video = half_court_video.astype(int).reshape(-1, 2)
+
+    # prinkt(court_corners_video, court_corners_video.shape)
     pt1 = court_corners_video[0, :]
-    print(pt1)
     pt2 = court_corners_video[1, :]
     pt3 = court_corners_video[2, :]
     pt4 = court_corners_video[3, :]
+
+
 
     cv2.line(img, pt1, pt2, (0, 0, 255), 3)
     cv2.line(img, pt2, pt3, (0, 0, 255), 3)
     cv2.line(img, pt3, pt4, (0, 0, 255), 3)
     cv2.line(img, pt4, pt1, (0, 0, 255), 3)
 
+    pt1 = right_key_video[0, :]
+    pt2 = right_key_video[1, :]
+    pt3 = right_key_video[2, :]
+    pt4 = right_key_video[3, :]
+
+    cv2.line(img, pt1, pt2, (0, 0, 255), 3)
+    cv2.line(img, pt2, pt3, (0, 0, 255), 3)
+    cv2.line(img, pt3, pt4, (0, 0, 255), 3)
+    cv2.line(img, pt4, pt1, (0, 0, 255), 3)
+
+    pt1 = half_court_video[0, :]
+    pt2 = half_court_video[1, :]
+
+    cv2.line(img, pt1, pt2, (0, 0, 255), 3)
+
     # print(court_corners_video)
     cv2.imshow('frame', img)
-    cv2.waitKey(0)
+    if cv2.waitKey(0) == ord('s'):
+        cv2.imwrite("C:/Users/simon/Downloads/homogpic.png", img)
+
 cv2.destroyAllWindows()
 
---
+++ just_inference.py
@@ -0,0 +1,87 @@
+'''
+new inference file that just does inference (original inference.py does
+evaluation as well which we can't do with bball)
+
+want to have input: image -> output: predicted homography matrix
+'''
+
+
+
+import torch
+import os
+import os.path as osp
+import numpy as np
+from PIL import Image
+from models.eval_network import EvalKpSFR
+import utils
+from models.inference_core import InferenceCore
+import cv2
+
+
+
+def class_mapping(rgb, template):
+    src_pts = rgb.copy()
+    cls_map_pts = []
+
+    for ind, elem in enumerate(src_pts):
+        coords = np.where(elem[2] == template[:, 2])[0]  # find correspondence
+        cls_map_pts.append(template[coords[0]])
+    dst_pts = np.array(cls_map_pts, dtype=np.float32)
+
+    return src_pts[:, :2], dst_pts[:, :2]
+
+def infer_homography(eval_model, image, device, template_grid):
+    with torch.no_grad():
+        processor = InferenceCore(eval_model, image, device, k=91, lookup=None)
+        processor.interact(0, image.shape[1], selector=None)
+
+        out_masks = processor.prob[:, 0]  # Assuming single frame input
+        out_scores, out_masks = torch.max(out_masks, dim=0)
+
+        out_masks = out_masks.detach().cpu().numpy()
+        out_scores = out_scores.detach().cpu().numpy()
+
+        image = np.transpose(image[0].detach().cpu().numpy(), (1, 2, 0))  # h*w*c
+        pred_keypoints = np.zeros_like(out_masks)
+
+        pred_rgb = []
+        for ind in range(1, 92):  # 1 to 91 classes
+            if np.any(out_masks == ind):
+                y, x = np.unravel_index(np.argmax(out_masks == ind), out_masks.shape)
+                pred_keypoints[y, x] = ind
+                pred_rgb.append([x * 4, y * 4, ind])
+
+        pred_rgb = np.asarray(pred_rgb, dtype=np.float32)
+        pred_homo = None
+
+        if pred_rgb.shape[0] >= 4:
+            src_pts, dst_pts = class_mapping(pred_rgb, template_grid)
+            pred_homo, _ = cv2.findHomography(src_pts.reshape(-1, 1, 2), dst_pts.reshape(-1, 1, 2), cv2.RANSAC, 10)
+
+        return pred_homo
+
+def main():
+    # Setup GPU or CPU
+    # device = torch.device('cuda')
+    device = torch.device('cpu')
+    # Load the model
+    weights_path = 'checkpoint/kpsfr_finetuned.pth'
+    eval_model = EvalKpSFR(model_archi='KC', num_objects=91, non_local=False).to(device)
+    checkpoint = torch.load(weights_path, map_location=device)
+    eval_model.load_state_dict(checkpoint['model_state_dict'])
+    eval_model.eval()
+    return
+
+    # Load the input image
+    input_image = Image.open('path_to_input_image.jpg')
+    input_tensor = utils.im_to_torch(input_image).unsqueeze(0).to(device)
+
+    # Generate the template grid
+    template_grid = utils.gen_template_grid_bball()
+
+    # Infer the homography
+    predicted_homography = infer_homography(eval_model, input_tensor, device, template_grid)
+    print("Predicted Homography:", predicted_homography)
+
+if __name__ == '__main__':
+    main()
